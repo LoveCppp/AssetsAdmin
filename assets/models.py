f1e4028632d2a35d962b10f7dacceb808d9e9c7b
t4mo <lg224@foxmail.com>
2017-06-02 03:18:10 -0400

    first commit

---
+++ assets/models.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+
+class Port(models.Model):
+    ip = models.CharField(max_length=255)
+    port = models.CharField(max_length=255)
+    port = models.CharField(max_length=255)
+    service = models.CharField(max_length=255)
+    product = models.CharField(max_length=255)
+    version = models.CharField(max_length=255)
+    createtime = models.DateTimeField()
+    class Meta:
+        db_table = 'portscan_main'
+
+
+class Iplist(models.Model):
+    ip = models.CharField(max_length=255)
+    port = modelslll.TextField()
+    createtime = models.DateTimeField()
+    class Meta:
+        db_table = 'iplist'
