60193d047aae2905151c81573366ccd73c053ae9
t4mo <lg224@foxmail.com>
2017-06-05 02:26:18 -0400

    update

--- AssetsAdmin/urls.py
+++ AssetsAdmin/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from admin import admin
+from admin import admin,portscan
 from django.conf.urls.static import static
 from django.conf import settings
 
 
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
-    url(r'^admin/', admin.index),
+    url(r'^admin/$', admin.index),
+    url(r'^admin/portscan/', portscan.index),
 
 ]
